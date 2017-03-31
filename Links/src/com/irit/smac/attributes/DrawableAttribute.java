203f6df048333524e9ef5589cd3c2c3c0777d0dc
Nicolas Verstaevel <n.verstaevel@gmail.com>
2017-03-31 15:49:37 +0200

    Adding jar file.

---
+++ Links/src/com/irit/smac/attributes/DrawableAttribute.java
@@ -0,0 +1,41 @@
+package com.irit.smac.attributes;
+
+import com.irit.smac.model.Attribute;
+
+public class DrawableAttribute {
+
+	public enum Type {
+		Agent, Relation;
+	};
+
+	private Type type;
+
+	private Attribute t;
+
+	private String name;
+
+	private String caracList;
+
+	public DrawableAttribute(Type type, String entityName, String caracList, Attribute t) {
+		this.type = type;
+		this.name = entityName;
+		this.caracList = caracList;
+		this.t = t;
+	}
+
+	public Type getType() {
+		return type;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getCaracList(){
+		return caracList;
+	}
+
+	public Attribute getAttribute() {
+		return t;
+	}
+}
